2fbc9543270bb797fa1ecc4f2cc28baff67a2aa8
slush62 <steve@lushfamily.com>
2020-06-01 15:11:28 -0500

    Initial spring.io

---
+++ src/main/java/com/peraton/geodata/jsonClasses/JsonClassesApplication.java
@@ -0,0 +1,13 @@
+package com.peraton.geodata.jsonClasses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonClassesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonClassesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/peraton/geodata/jsonClasses/JsonClassesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peraton.geodata.jsonClasses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonClassesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
